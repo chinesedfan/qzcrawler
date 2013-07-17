3790940977b1cfc4d27f5baf582b7236e66321f9
chinesedfan <chinesedfan@qq.com>
2013-07-17 12:35:00 +0000

    finish scratch.py
    git-svn-id: svn://localhost/qzcrawler@19 6f648cbf-d750-cb45-8548-4806c1bf26f8

--- src/main.py
+++ src/main.py
@@ -10,7 +10,10 @@ def main():
     #scratch_msgboard("../raw/msgboard.txt")
     #scratch_bloglist("../raw/bloglist.txt")
     #scratch_shuoshuo("../raw/shuoshuo.txt")
-    scratch_albumlist("../raw/albumlist.txt")   
+    #scratch_albumlist("../raw/albumlist.txt")
+    #scratch_blogcmt("1226814446", "../raw/blogcmt.txt", True)   
+    #scratch_photolist("318908369", "../raw/photolist.txt", True)
+    scratch_photocmt("318908369", "NDJ0n1.5FWCUkFHgzZsB.d*8X8AjAAA!", "../raw/photocmt.txt", True)    
     
 if __name__ == '__main__':
     main()

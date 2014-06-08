b552bfd6f621d21ae16660a0aa7d125cbaf40a2c
chinesedfan <chinesedfan@qq.com>
2014-06-08 13:24:56 +0800

    rewrite pool.py

--- src/pool.py
+++ src/pool.py
@@ -8,50 +8,47 @@ pool.py - Maintain HTTP/HTTPS connections
 import httplib
 import sys
 
-import config
-
-http_pool = {}
-https_pool = {}
-
-def get_http_connection(host):
-    return get_connection_template("http", host)
-
-def get_https_connection(host):
-    return get_connection_template("https", host)
-
-'''
-Internal helper functions
-'''        
-
-def get_connection_template(kind, host):
-    global http_pool, https_pool
-    f = None
-    pool = {}
-    
-    print "try to get %s connection to %s..." % (kind, host)
-    if kind == "http":
-        f = httplib.HTTPConnection
-        pool = http_pool
-    elif kind == "https":
-        f = httplib.HTTPSConnection
-        pool = https_pool
+class BasePool(object):
+    def __init__(self, name, func, timeout=2, retry=5):
+        self.pool = {}
+        self.name = name
+        self.func = func
+        self.timeout = timeout
+        self.retry = retry
         
-    if not host in pool:
-        # retry several times
-        for i in range(config.retry):
-            try:
-                conn = f(host, timeout = config.timeout)
-                conn.connect()
+    def get_instance(self, host):
+        if self.func is None:
+            return None
+
+        if not host in self.pool:
+            for i in range(self.retry):
+                try:
+                    conn = self.func(host, timeout = self.timeout)
+                    conn.connect()
                 
-                print "...create %s connection to %s" % (kind, host)
-                pool[host] = conn
-                break
-            except:
-                print sys.exc_info()
-                if i == config.retry - 1:
-                    print "...abort to retry, exit"
-                    sys.exit()
-                print "...retry, i=%d" % i
-                continue                    
-    print "...done."
-    return pool[host]
+                    print "...create %s connection to %s" % (self.name, host)
+                    self.pool[host] = conn
+                    break
+                except:
+                    print sys.exc_info()
+                    if i == self.retry - 1:
+                        print "...abort to retry, exit"
+                        sys.exit()
+                    print "...retry, i=%d" % i
+                    continue                    
+        print "...done."
+        return self.pool[host]
+
+class HttpPool(BasePool):
+    def __init__(self, timeout=2, retry=5):
+        super(HttpPool, self).__init__("HTTP", httplib.HTTPConnection, timeout, retry)
+        
+
+class HttpsPool(BasePool):
+    def __init__(self, timeout=2, retry=5):
+        super(HttpPool, self).__init__("HTTPS", httplib.HTTPSConnection, timeout, retry)
+
+# For testing
+if __name__ == '__main__':
+    pool = HttpPool()
+    pool.get_instance("www.baidu.com")

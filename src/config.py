233c2833ecd84ed13b8873170f6dd6481dc8e91b
chinesedfan <chinesedfan@qq.com>
2014-06-08 14:38:36 +0800

    rewrite config.py

--- src/config.py
+++ src/config.py
@@ -5,40 +5,46 @@
 config.py - Global configuration
 '''
 
-import login
+from login import LoginUtil
 
-# Attention:
-#     Don't store config.xxx in other modules, or changes of
-#     config.xxx would not take effects.
+class Config(object):
+    def __init__(self, qq="123456789", pwd="test", isLogin=False):
+        # account ids
+        self.H_QQ = qq
+        self.G_QQ = qq
 
-# account ids
-H_QQ = "123456789"
-G_QQ = "123456789"
+        # authorization information
+        self.PWD = pwd
+        if isLogin:
+            self.G_TK, self.COOKIE = LoginUtil.main(self.H_QQ, self.PWD)
 
-# authorization information
-PWD = "testpwd"
-G_TK = "1234567890"
+        # raw file coding
+        self.code = "gbk"
 
-# raw file coding
-code = "gbk"
+        # scratch related
+        self.rawfile = {
+            "msgboard": "../raw/msgboard.txt",
+            "bloglist": "../raw/bloglist.txt",    
+            "shuoshuo": "../raw/shuoshuo.txt",
+            "albumlist": "../raw/albumlist.txt",
+            # reuse these files, override every time
+            "blogcmt": "../raw/blogcmt.txt",
+            "photolist": "../raw/photolist.txt",
+            "photocmt": "../raw/photocmt.txt",
+            # avoid to override, and directories should end with slash
+            "blogdir": "../raw/blog/",
+            "albumdir": "../raw/album/",   
+        }
+        self.retry = 5    # retry times
+        self.timeout = 2  # timeout for web operations
 
-# scratch related
-rawfile = {
-    "msgboard": "../raw/msgboard.txt",
-    "bloglist": "../raw/bloglist.txt",    
-    "shuoshuo": "../raw/shuoshuo.txt",
-    "albumlist": "../raw/albumlist.txt",
-    # reuse these files, override every time
-    "blogcmt": "../raw/blogcmt.txt",
-    "photolist": "../raw/photolist.txt",
-    "photocmt": "../raw/photocmt.txt",
-    # avoid to override, and directories should end with slash
-    "blogdir": "../raw/blog/",
-    "albumdir": "../raw/album/",   
-}
-retry = 5    # retry times
-timeout = 2  # timeout for web operations
+        # database related
+        self.dbfile = "../db/qzone.db"
+        self.sqlfile = "../db/db.sql"
 
-# database related
-dbfile = "../db/qzone.db"
-sqlfile = "../db/db.sql"
+    def updateLoginInfo(self):
+        self.G_TK, self.COOKIE = LoginUtil.main(self.H_QQ, self.PWD)
+
+# For testing
+if __name__ == '__main__':
+    config = Config(isLogin=True);

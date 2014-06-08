bfa0373776195c80879e21e1d492821612b27409
chinesedfan <chinesedfan@qq.com>
2014-06-08 15:16:51 +0800

    rewrite analyze.py

--- src/analyze.py
+++ src/analyze.py
@@ -7,148 +7,156 @@ analyze.py - Analysis from the scratched file, and save into the database
 
 import json
 
-import db
-import config
+from db import CrawlerDb
+from config import Config
 
-def analyze_msgboard(logfile):
-    for s in get_jsonstr(logfile):
-        data = json.loads(s)
-        
-        cmtlst = data["data"]["commentList"]        
-        print "got commentList=%d" % len(cmtlst)
-        for cmt in cmtlst:
-            rpllst = cmt["replyList"]
-            db.insert_msgboard(cmt["id"], cmt["uin"], json.dumps(cmt["ubbContent"], ensure_ascii=False),
-                               cmt["pubtime"], cmt["modifytime"], len(rpllst))
-    
-            print "got replyList=%d" % len(rpllst)
-            for i in range(len(rpllst)):
-                rpl = rpllst[i]
-                db.insert_msgreply(cmt["id"], i, rpl["uin"], 
-                                   rpl["content"], rpl["time"])
-    db.execute_commit()
-    print "...done."
+class Analyzer(object):
+    def __init__(self, config):
+        self.config = config
+        self.db = CrawlerDb(config.dbfile)
 
-def analyze_bloglist(logfile):
-    for s in get_jsonstr(logfile):
-        data = json.loads(s)
-        
-        bloglst = data["data"]["list"]        
-        print "got bloglst=%d" % len(bloglst)
-        for blog in bloglst:
-            db.insert_bloglist(blog["blogId"], blog["cate"], blog["title"], 
-                               blog["pubTime"], blog["commentNum"])    
-    db.execute_commit()
-    print "...done."
-
-def analyze_blogcmt(blogid, logfile):
-    for s in get_jsonstr(logfile):
-        data = json.loads(s)
-        
-        cmtlst = data["data"]["comments"]        
-        print "got commentList=%d" % len(cmtlst)
-        for cmt in cmtlst:
-            rpllst = cmt["replies"]
-            db.insert_blogcmt(blogid, cmt["id"], cmt["poster"]["id"], json.dumps(cmt["content"], ensure_ascii=False),
-                               cmt["postTime"], len(rpllst))
-    
-            print "got replyList=%d" % len(rpllst)
-            for rpl in rpllst:
-                db.insert_blogreply(blogid, cmt["id"], rpl["id"], rpl["poster"]["id"], 
-                                   rpl["content"], rpl["postTime"])    
-    db.execute_commit()
-    print "...done."
-
-def analyze_shuoshuo(logfile):
-    for s in get_jsonstr(logfile):
-        data = json.loads(s)
-        
-        msglst = data["msglist"]
-        print "got msgList=%d" % len(msglst)
-        for msg in msglst:
-            db.insert_sslist(msg["tid"], msg["content"], msg["createTime"], msg["cmtnum"])
-            if not "commentlist" in msg:
-                continue       
+    def analyze_msgboard(self, logfile):
+        for s in self.get_jsonstr(logfile):
+            data = json.loads(s)
             
-            # someone adds comments on this message
-            cmtlst = msg["commentlist"]
+            cmtlst = data["data"]["commentList"]        
             print "got commentList=%d" % len(cmtlst)
             for cmt in cmtlst:
-                db.insert_sscmt(msg["tid"], cmt["tid"], cmt["uin"], cmt["content"],
-                                   cmt["createTime"], cmt["reply_num"])
+                rpllst = cmt["replyList"]
+                self.db.insert_msgboard(cmt["id"], cmt["uin"], json.dumps(cmt["ubbContent"], ensure_ascii=False),
+                                   cmt["pubtime"], cmt["modifytime"], len(rpllst))
         
-                if not "list_3" in cmt:
-                    continue
-                
-                # someone replies on this comment            
-                rpllst = cmt["list_3"]
+                print "got replyList=%d" % len(rpllst)
+                for i in range(len(rpllst)):
+                    rpl = rpllst[i]
+                    self.db.insert_msgreply(cmt["id"], i, rpl["uin"], 
+                                       rpl["content"], rpl["time"])
+        self.db.execute_commit()
+        print "...done."
+
+    def analyze_bloglist(self, logfile):
+        for s in self.get_jsonstr(logfile):
+            data = json.loads(s)
+            
+            bloglst = data["data"]["list"]        
+            print "got bloglst=%d" % len(bloglst)
+            for blog in bloglst:
+                self.db.insert_bloglist(blog["blogId"], blog["cate"], blog["title"], 
+                                   blog["pubTime"], blog["commentNum"])    
+        self.db.execute_commit()
+        print "...done."
+
+    def analyze_blogcmt(self, blogid, logfile):
+        for s in self.get_jsonstr(logfile):
+            data = json.loads(s)
+            
+            cmtlst = data["data"]["comments"]        
+            print "got commentList=%d" % len(cmtlst)
+            for cmt in cmtlst:
+                rpllst = cmt["replies"]
+                self.db.insert_blogcmt(blogid, cmt["id"], cmt["poster"]["id"], json.dumps(cmt["content"], ensure_ascii=False),
+                                   cmt["postTime"], len(rpllst))
+        
                 print "got replyList=%d" % len(rpllst)
                 for rpl in rpllst:
-                    db.insert_ssreply(msg["tid"], cmt["tid"], rpl["tid"], rpl["uin"], 
-                                       rpl["content"], rpl["createTime"])
-    db.execute_commit()
-    print "...done."
+                    self.db.insert_blogreply(blogid, cmt["id"], rpl["id"], rpl["poster"]["id"], 
+                                       rpl["content"], rpl["postTime"])    
+        self.db.execute_commit()
+        print "...done."
 
-def analyze_albumlist(logfile):
-    for s in get_jsonstr(logfile):
-        data = json.loads(s)
+    def analyze_shuoshuo(self, logfile):
+        for s in self.get_jsonstr(logfile):
+            data = json.loads(s)
+            
+            msglst = data["msglist"]
+            print "got msgList=%d" % len(msglst)
+            for msg in msglst:
+                self.db.insert_sslist(msg["tid"], msg["content"], msg["createTime"], msg["cmtnum"])
+                if not "commentlist" in msg:
+                    continue       
+                
+                # someone adds comments on this message
+                cmtlst = msg["commentlist"]
+                print "got commentList=%d" % len(cmtlst)
+                for cmt in cmtlst:
+                    self.db.insert_sscmt(msg["tid"], cmt["tid"], cmt["uin"], cmt["content"],
+                                       cmt["createTime"], cmt["reply_num"])
+            
+                    if not "list_3" in cmt:
+                        continue
+                    
+                    # someone replies on this comment            
+                    rpllst = cmt["list_3"]
+                    print "got replyList=%d" % len(rpllst)
+                    for rpl in rpllst:
+                        self.db.insert_ssreply(msg["tid"], cmt["tid"], rpl["tid"], rpl["uin"], 
+                                           rpl["content"], rpl["createTime"])
+        self.db.execute_commit()
+        print "...done."
+
+    def analyze_albumlist(self, logfile):
+        for s in self.get_jsonstr(logfile):
+            data = json.loads(s)
+            
+            albumlst = data["data"]["album"]        
+            print "got albumlst=%d" % len(albumlst)
+            for album in albumlst:
+                self.db.insert_albumlist(album["id"], album["name"], album["desc"], album["createtime"],
+                                    album["lastuploadtime"], album["modifytime"], album["total"])    
+        self.db.execute_commit()
+        print "...done."
+
+    def analyze_photolist(self, albumid, logfile):
+        for s in self.get_jsonstr(logfile):
+            data = json.loads(s)
+            
+            photolst = data["data"]["photoList"]
+            # no photo in this album
+            if photolst is None:
+                break
+            print "got photolst=%d" % len(photolst)
+            for photo in photolst:
+                self.db.insert_photolist(albumid, photo["lloc"], photo["name"], photo["desc"], 
+                                    photo["uploadtime"], photo["forum"])
+        self.db.execute_commit()
+        print "...done."  
+
+    def analyze_photocmt(self, albumid, photoid, logfile):
+        for s in self.get_jsonstr(logfile):
+            data = json.loads(s)
+            
+            cmtlst = []
+            if "comments" in data["data"]:
+                cmtlst = data["data"]["comments"]        
+            print "got commentList=%d" % len(cmtlst)
+            for cmt in cmtlst:
+                rpllst = []
+                if "replies" in cmt:
+                    rpllst = cmt["replies"]
+                self.db.insert_photocmt(albumid, photoid, cmt["id"], cmt["poster"]["id"], cmt["content"],
+                                   cmt["postTime"], len(rpllst))
         
-        albumlst = data["data"]["album"]        
-        print "got albumlst=%d" % len(albumlst)
-        for album in albumlst:
-            db.insert_albumlist(album["id"], album["name"], album["desc"], album["createtime"],
-                                album["lastuploadtime"], album["modifytime"], album["total"])    
-    db.execute_commit()
-    print "...done."
+                print "got replyList=%d" % len(rpllst)
+                for rpl in rpllst:
+                    self.db.insert_photoreply(albumid, photoid, cmt["id"], rpl["id"], rpl["poster"]["id"], 
+                                       rpl["content"], rpl["postTime"])    
+        self.db.execute_commit()
+        print "...done."
 
-def analyze_photolist(albumid, logfile):
-    for s in get_jsonstr(logfile):
-        data = json.loads(s)
+    def get_jsonstr(self, logfile):
+        f = open(logfile)
+        content = f.read().decode(self.config.code) # Use the saved coding
         
-        photolst = data["data"]["photoList"]
-        # no photo in this album
-        if photolst is None:
-            break
-        print "got photolst=%d" % len(photolst)
-        for photo in photolst:
-            db.insert_photolist(albumid, photo["lloc"], photo["name"], photo["desc"], 
-                                photo["uploadtime"], photo["forum"])
-    db.execute_commit()
-    print "...done."  
+        lst = []
+        callbacks = content.split("_Callback")
+        for i in range(1, len(callbacks)):
+            lst.append(callbacks[i][1:-3])
+        print "get_jsonstr size=%d" % len(lst)
+        return lst
 
-def analyze_photocmt(albumid, photoid, logfile):
-    for s in get_jsonstr(logfile):
-        data = json.loads(s)
-        
-        cmtlst = []
-        if "comments" in data["data"]:
-            cmtlst = data["data"]["comments"]        
-        print "got commentList=%d" % len(cmtlst)
-        for cmt in cmtlst:
-            rpllst = []
-            if "replies" in cmt:
-                rpllst = cmt["replies"]
-            db.insert_photocmt(albumid, photoid, cmt["id"], cmt["poster"]["id"], cmt["content"],
-                               cmt["postTime"], len(rpllst))
-    
-            print "got replyList=%d" % len(rpllst)
-            for rpl in rpllst:
-                db.insert_photoreply(albumid, photoid, cmt["id"], rpl["id"], rpl["poster"]["id"], 
-                                   rpl["content"], rpl["postTime"])    
-    db.execute_commit()
-    print "...done."
-
-'''
-Internal helper functions
-'''
-
-def get_jsonstr(logfile):
-    f = open(logfile)
-    content = f.read().decode(config.code) # Use the saved coding
-    
-    lst = []
-    callbacks = content.split("_Callback")
-    for i in range(1, len(callbacks)):
-        lst.append(callbacks[i][1:-3])
-    print "get_jsonstr size=%d" % len(lst)
-    return lst
+# For testing
+if __name__ == '__main__':
+    config = Config("123456789", "test", True)
+    config.dbfile = "../db/test.db"
+    analyzer = Analyzer(config)
+    analyzer.analyze_msgboard(config.rawfile["msgboard"])
